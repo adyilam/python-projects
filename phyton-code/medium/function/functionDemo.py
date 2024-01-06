ac67a94e4dd163318d9bdee9cfa71ab3250b7787
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2024-01-05 23:08:22 -0500

    lambda funtion

--- phyton-code/medium/function/functionDemo.py
+++ phyton-code/medium/function/functionDemo.py
@@ -199,3 +199,34 @@ isEmployee = isinstance(employee, Employee)
 isEmployee1 = isinstance(list_, Employee)
 print("Is it employee instance: ", isEmployee)
 print("Is it employee instance: ", isEmployee1)
+
+
+# Lambda Functions in Python are anonymous functions,function don't have a name
+# Lambda keyword in Python to define an unnamed function. like def key word  used to
+# create python function
+# syntax  lambda arguments: expression
+def multiply(x, y):
+    return x * y
+
+
+print(multiply(2, 3))
+
+# multiply function using lambda
+value = lambda x, y: (x * y)
+print(value(4, 5))
+
+# filter even numbers from the list of integers, using filter()
+list_ = [2, 4, 5, 6, 7, 8, 9]
+odd_numbers = list(filter(lambda n: (n % 2 == 0), list_))
+print("List of odd numbers: ", odd_numbers)
+
+# find the square of numbers in the list using map()
+list_ = [2, 4, 5, 6, 7, 8, 9]
+square_of_a_number = list(map(lambda n: (n * 2), list_))
+print("The square of the number ", list_, " is:   ", square_of_a_number)
+
+# lambda function combined with list comprehension and the lambda keyword with a for loop.
+squares_of_each_number = [lambda num=num: num ** 2 for num in range(0, 11)]
+print('The square value of all numbers from 0 to 10:')
+for square in squares_of_each_number:
+    print(square(), end=" ")
