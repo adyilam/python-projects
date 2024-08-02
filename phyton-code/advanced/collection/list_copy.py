f6d12c6d3b3d6faa335651c74b522ea3371e4e42
Aderajew Amare <ade@Aderajews-MacBook-Pro-2.local>
2024-08-02 18:28:05 -0400

    list sort , join and sort list added

---
+++ phyton-code/advanced/collection/list_copy.py
@@ -0,0 +1,4 @@
+# copy list
+name_list = ["Smith", "Tom", "Zack", "Bob"]
+newList = name_list.copy()
+print(newList)
---
+++ phyton-code/advanced/collection/list_sort.py
@@ -0,0 +1,16 @@
+# list sort, sorts the list alphabetically
+name_list = ["Smith", "Tom", "Zack", "Bob"]
+name_list.sort()
+print(name_list)
+
+# list sort, sorts the list numerically
+nums = [10, 8, 4, 2, 3, 6, 1, 5]
+nums.sort()
+print(nums)
+
+# sort the list descending order
+name_list = ["Smith", "Tom", "Zack", "Bob"]
+name_list.sort(reverse=True)
+print(name_list)
+
+
