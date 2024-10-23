141b6971f67b115b96edf690b3059604b95f595e
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2024-10-23 15:27:14 -0400

    added more list and filter changes

--- phyton-code/advanced/collection/list_comprehension2.py
+++ phyton-code/advanced/collection/list_comprehension2.py
@@ -68,10 +68,44 @@ for x in range(1, 11):
 print(power_of_2)
 
 # print multiplication table
+print("Multiplication Table: ")
 a = 6
 multi_table = [[a, b, a * b] for b in range(1, 5)]
-print(multi_table)
+# print(multi_table)
 
 # display multiplication table
 for i in multi_table:
     print(i)
+
+# Division table
+print("Division Table: ")
+a = 2
+div_table = [[a, b, a / b] for b in range(1, 5)]
+for i in div_table:
+    print(i)
+
+# slicing
+#  below list has numbers from 6 to 8
+lst = list(range(1, 11))
+lst5_8 = lst[5: 8]
+print(lst5_8)
+
+#  below list has numbers from 2 to 10
+lst1_ = lst[1:]
+print(lst1_)
+
+#  below list has numbers from 1 to 5
+lst_5 = lst[: 5]
+print(lst_5)
+
+#  below list has numbers from 2 to 8 in step 2
+lst1_8_2 = lst[1: 8: 2]
+print(lst1_8_2)
+
+#  below list has numbers from 10 to 1
+lst_rev = lst[:: -1]
+print(lst_rev)
+
+#  below list has numbers from 10 to 6 in step 2
+lst_rev_9_5_2 = lst[9: 4: -2]
+print(lst_rev_9_5_2)
