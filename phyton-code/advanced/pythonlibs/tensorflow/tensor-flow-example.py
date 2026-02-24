62f6e9c65db953d274247a7707dfbd8a0cc13335
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2026-02-23 20:00:30 -0500

    numpy example

--- phyton-code/advanced/pythonlibs/tensorflow/tensor-flow-example.py
+++ phyton-code/advanced/pythonlibs/tensorflow/tensor-flow-example.py
@@ -1,9 +1,24 @@
+"""
+Training a simple linear model to learn:
+y = 2x - 1
+"""
 import numpy as np
-from tensorflow.keras import sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
 
-model = sequential([Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sqd', loss='mean_square_error')
+model = Sequential([
+    Input(shape=(1,)),  # Recommended way
+    Dense(units=1)
+])
+
+model.compile(
+    optimizer='sgd',
+    loss='mean_squared_error'
+)
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500, verbose=0)
+
+print(model.predict(np.array([[10.0]])))
